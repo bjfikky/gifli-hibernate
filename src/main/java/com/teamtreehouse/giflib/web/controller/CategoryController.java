a26f325f8a1bd9174a6ee85779fe57fd91c25996
Benjamin Orinoloye <bjfikky@yahoo.com>
2017-04-14 14:27:19 -0400

    added validation to category

--- src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/CategoryController.java
@@ -8,12 +8,14 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -75,8 +77,10 @@ public class CategoryController {
 
     // Add a category
     @RequestMapping(value = "/categories", method = RequestMethod.POST)
-    public String addCategory(Category category) {
+    public String addCategory(@Valid Category category, BindingResult result) {
         // TODO: Add category if valid data was received
+        if (result.hasErrors()) return "redirect: categories/add";
+
         categoryService.save(category);
 
         // TODO: Redirect browser to /categories
